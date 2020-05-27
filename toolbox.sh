1d19a1a535b3999911d40e5a77dc22196f46ca68
dimo5991 <65290591+dimo5991@users.noreply.github.com>
2020-05-27 18:06:52 +0200

    Add files via upload

---
+++ toolbox.sh
@@ -0,0 +1,166 @@
+a="\033[1;30m"
+b="\033[1;31m"
+c="\033[1;32m"
+d="\033[1;33m"
+e="\033[1;34m"
+   
+
+
+
+
+                                 figlet "TOOL BOX"
+
+
+
+
+
+
+                    echo "            \033[1;34m               created by ||DIMO5991|| 💚"
+
+
+
+echo "     1-Nikto"
+echo "     2-Nmap"
+echo "     3-Metagoofil"
+echo "     4-Sparta"
+echo "     5-PhoneInfoga"
+echo "     6-InfoG"
+echo "     7-URLextractor"
+echo "     8-DARK-EAGLE"
+echo "     9-GatheTOOL"
+echo "     10-Information-Gathering"
+echo "     11-Aircrack-ng"
+echo "     12-Wifite"
+echo "     13-Wifiphisher"
+echo "     14-Wifi-honey"
+echo "     15-Kismet"
+echo "     16-Fern-wifi-cracker"
+echo "     17-Skipfish"
+echo "     18-Powerfuzzer"
+echo "     19-WhatWeb"
+echo "     20-Wpscan"
+echo "     21-Wfuzz"
+echo "     22-Websploit"
+echo "     23-Sqlninja"
+echo "     24-Joomscan"
+echo "     25-Jsql"
+echo "     26-sqlmap"
+echo "     27-zaproxy"
+echo "     28-commix"
+echo "     29-msfpc"
+echo "     30-backdoor-factory"
+echo "     31-metaSploit"
+echo "     31-beef"
+echo "     32-backdoor.apk"
+echo "     33-TheFatRat"
+echo "     34-A-Rat"
+echo "     35-xerosploit"
+echo "     36-routersploir"
+echo "     37-hulk"
+echo "     38-4nonimizer"
+echo "     39-ATSCAN"
+echo "     40-Androspy"
+echo "     41-hackall"
+echo "     42-bettercap"
+echo "     43-sniffjoke"
+echo "     44-hashcat"
+echo "     45-JohnTheRipper"
+echo "     46-johnny"
+echo "     47-ncrack"
+echo "     48-hydra"
+echo "     49-findmyhash"
+echo "     50-D-TECT"
+echo "     51-Dr0p1t-Framework"
+echo "     52-Email-Spammer"
+echo "     53-Empire"
+echo "     54-EvilURL"
+echo "     55-ExploitOnCLI"
+echo "     56-FakeImageExploite"
+echo "     57-Findsploit"
+echo "     58-Gemail-Hack"
+echo "     59-Instahack"
+echo "     60-KnockMail"
+echo "     61-LITEDDOS"
+echo "     62-LITESPAM"
+echo "     63-PowerSploit"
+echo "     64-SocialFish"
+
+
+
+
+
+
+git clone https://github.com/sullo/nikto.git
+git clone https://github.com/nmap/nmap
+git clone https://github.com/laramies/metagoofil.git
+git clone https://github.com/secforce/sparta.git
+git clone https://github.com/sundowndev/PhoneInfoga.git
+git clone https://github.com/thelinuxchoice/infog
+git clone https://github.com/eschultze/URLextractor.git
+git clone https://github.com/nandydark/DARK-EAGLE.git 
+git clone https://github.com/AngelSecurityTeam/GatheTOOL
+git clone https://github.com/kaitolegion/Information-Gathering.git
+git clone https://github.com/aircrack-ng/aircrack-ng.git
+git clone https://github.com/derv82/wifite.git
+git clone https://github.com/wifiphisher/wifiphisher.git
+git clone https://github.com/samothrakes/wifi-honey.git
+git clone https://github.com/kismetwireless/kismet.git
+git clone https://github.com/savio-code/fern-wifi-cracker.git
+git clone https://github.com/spinkham/skipfish.git
+git clone https://github.com/marcinguy/powerfuzzer.git
+git clone https://github.com/urbanadventurer/WhatWeb.git
+git clone https://github.com/wpscanteam/wpscan.git
+git clone https://github.com/xmendez/wfuzz.git
+git clone https://github.com/websploit/websploit.git
+git clone https://github.com/xxgrunge/sqlninja.git
+git clone https://github.com/rezasp/joomscan.git
+git clone https://github.com/ron190/jsql-injection.git
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/zaproxy/zaproxy.git
+git clone https://github.com/commixproject/commix.git
+git clone https://github.com/g0tmi1k/msfpc.git
+git clone https://github.com/secretsquirrel/the-backdoor-factory.git
+git clone https://github.com/rapid7/metasploit-framework
+git clone https://github.com/beefproject/beef
+git clone https://github.com/dana-at-cp/backdoor-apk
+git clone https://github.com/Screetsec/TheFatRat
+git clone https://github.com/Xi4u7/A-Rat
+git clone https://github.com/LionSec/xerosploit
+git clone https://github.com/threat9/routersploit
+git clone https://github.com/grafov/hulk
+git clone https://github.com/Hackplayers/4nonimizer.git
+git clone https://github.com/AlisamTechnology/ATSCAN.git
+git clone https://github.com/TunisianEagles/Androspy.git
+git clone https://github.com/dimo5991/hackall.git
+git clone https://github.com/bettercap/bettercap.git
+git clone https://github.com/evilaliv3/sniffjoke.git
+git clone https://github.com/hashcat/hashcat.git
+git clone https://github.com/magnumripper/JohnTheRipper.git
+git clone https://github.com/j2sg/johnny.git
+git clone https://github.com/nmap/ncrack.git
+git clone https://github.com/vanhauser-thc/thc-hydra.git
+git clone https://github.com/Talanor/findmyhash.git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT-1.git
+git clone https://github.com/D4Vinci/Dr0p1t-Framework.git
+git clone https://github.com/Juniorn1003/Email-Spammer.git
+git clone https://github.com/EmpireProject/Empire.git
+git clone https://github.com/UndeadSec/EvilURL.git
+git clone https://github.com/r00tmars/ExploitOnCLI.git
+git clone https://github.com/r00t-3xp10it/FakeImageExploiter.git
+git clone https://github.com/1N3/Findsploit.git
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+git clone https://github.com/avramit/Instahack.git
+git clone https://github.com/4w4k3/KnockMail.git
+git clone https://github.com/4L13199/LITEDDOS.git
+git clone https://github.com/4L13199/LITESPAM.git
+git clone https://github.com/PowerShellMafia/PowerSploit.git
+git clone https://github.com/UndeadSec/SocialFish.git
+
+
+
+
+
+                      echo "End Installed all tool Start Hacking 👿"
+
+
+            figlet "||DIMO5991||"
